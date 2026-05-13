9df3985e472d60aa544f8a2510743f2cfc88b36e
Srushti Thombre <srushtiprajakt.t@gmail.com>
2026-05-13 13:30:48 +0530

    Deploying to railway

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
     cookie: { maxAge: 24 * 60 * 60 * 1000, httpOnly: true, secure: false },
   })
 );
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Static
 app.get("/favicon.ico", (req, res) => res.status(204).end());
@@ -224,4 +224,4 @@ app.use((err, req, res, next) => {
 });
 
 // Start
-app.listen(port, () => console.log(`http://localhost:${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
